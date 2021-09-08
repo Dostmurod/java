330a1b11df7e5c8442a0579e0aaa543433575cad
Dostmurod <dostmurodzoirov777@gmail.com>
2021-09-08 12:45:36 +0500

    java

---
+++ index1.js
@@ -0,0 +1,34 @@
+//let box = "dostmurod00"
+//console.log(box)
+//console.log(box)
+
+//let names = "bekmurod"
+//console.log(names)
+//console.log(names)
+//let pro = +prompt("Yoshingizni Kiriting nechanchi yilda tug'ilganingizni bilib beraman")
+//alert (2021-pro);
+//let sultan = confirm ("Sultonmurod")
+//console.log (sultan)
+//let arr = ["aplle",45,"iPhone"]
+//arr.push("arr")
+//arr.pop("iPhone")
+//arr.shift("iPhone")
+//arr.unshift("aplle")
+
+//console.log(arr)
+
+//let lesson = prompt["nok","anor","shaftmoli","o'rik","gilos","olma","iPhone"]
+//alert(01234567 -lesson)
+//console.log(lesson)
+
+let sovga = +prompt (["Kalkulator"])
+alert (10-  sovga)
+alert (10+  sovga)
+alert (10*  sovga)
+alert (10/ sovga)
+console.log(sovga)
+
+
+
+
+
